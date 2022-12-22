7d6c43108305dd3301dc1580967034dacc0dd8c7
Muhammed Ali Kurtulbaş <muhammedalikurtulbas@gmail.com>
2022-12-22 15:24:39 +0300

    Difficulty selection screen added

---
+++ Assets/Scripts/MainMenuScripts/MainMenuManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject difficultyScreen;
+    [SerializeField] private Vector3 endTransform = new Vector3(0f, 0f, 0f), startTransform = new Vector3(0f, -960f, 0f);
+    [SerializeField] private float LerpValue = 0f;
+    
+    public void NewGameButton()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(SetOnDifficultyScreen(startTransform, endTransform, 1f));
+    }
+
+    public void CloseDifficultyButton()
+    {
+        StartCoroutine(SetOffDifficultyScreen(endTransform, startTransform, 1f));
+    }
+
+    IEnumerator SetOnDifficultyScreen(Vector3 v_start, Vector3 v_end, float v_duration)
+    {
+        difficultyScreen.SetActive(true);
+        float elapsed = 0f;
+        while(elapsed < v_duration)
+        {
+            LerpValue = Mathf.Lerp(0f, 1f, elapsed / v_duration);
+            difficultyScreen.GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(v_start, v_end, LerpValue);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        difficultyScreen.GetComponent<RectTransform>().anchoredPosition = endTransform;
+    }
+
+    IEnumerator SetOffDifficultyScreen(Vector3 v_start, Vector3 v_end, float v_duration)
+    {
+        float elapsed = 0f;
+        while (elapsed < v_duration)
+        {
+            LerpValue = Mathf.Lerp(0f, 1f, elapsed / v_duration);
+            difficultyScreen.GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(v_start, v_end, LerpValue);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        difficultyScreen.GetComponent<RectTransform>().anchoredPosition = startTransform;
+        difficultyScreen.SetActive(false);
+    }
+}
